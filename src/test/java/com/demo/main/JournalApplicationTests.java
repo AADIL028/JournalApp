88b7eb4495c0d2773c5bd66fce2fca5393f9e46e
AADIL028 <112774372+AADIL028@users.noreply.github.com>
2024-06-10 15:59:19 +0530

    disable test.

--- src/test/java/com/demo/main/JournalApplicationTests.java
+++ src/test/java/com/demo/main/JournalApplicationTests.java
@@ -1,11 +1,13 @@
 package com.demo.main;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JournalApplicationTests {
 
+	@Disabled
 	@Test
 	void contextLoads() {
 	}
